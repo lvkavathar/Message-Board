072ccf37241be04dae882e8a8ceecea2a5e6dc2a
venkat-lingala <lvenkatkumarreddy@gmail.com>
2017-06-09 22:02:14 -0400

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,197 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var bodyParser = require("body-parser");
-var messages = [{text:'some text',owner:'john'},{text:'some',owner:'jack'}];
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+
+var messages = [{ text: 'Good to see you guys', owner: 'Venkat' }, { text: 'See you dude', owner: 'Moki' }];
+var users = [{firstName: 'a', email: 'a', password: 'a', id: 0}];
+
 app.use(bodyParser.json());
-app.use((req,res,next) => {
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Headers","Origin,X-Requested-with,Content-Type,Accept");
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
-} );
+})
 
 var api = express.Router();
-api.get("/messages",(req,res) => {
+var auth = express.Router();
+
+
+api.get('/messages', (req, res) => {
     res.json(messages);
-});
-api.get("/messages/:user",(req,res) => {
+})
+
+api.get('/messages/:user', (req, res) => {
     var user = req.params.user;
-    var result= messages.filter(message => message.owner == user);
+    var result = messages.filter(message => message.owner == user);
+
     res.json(result);
-});
-api.post("/message",(req,res) => {
+})
+
+api.post('/messages', (req, res) => {
     messages.push(req.body);
     res.json(req.body);
-});
-app.use('/api',api);
-app.listen(4505);
+})
+
+api.get('/users/me', checkAuthenticated, (req,res) => {
+    res.json(users[req.user]);
+})
+
+api.post('/users/me', checkAuthenticated, (req,res) => {
+    var user = users[req.user];
+
+    user.firstName = req.body.firstName;
+    user.lastName = req.body.lastName;
+
+    res.json(user);
+})
+
+auth.post('/login', (req, res) => {
+    var user = users.find(user => user.email == req.body.email);
+
+    if (!user) 
+        sendAuthError(res);
+
+    if (user.password == req.body.password)
+        sendToken(user, res);
+    else
+        sendAuthError(res);
+})
+
+auth.post('/register', (req, res) => {
+    var index = users.push(req.body) - 1;
+
+    var user = users[index];
+    user.id = index;
+
+    sendToken(user, res);
+})
+
+function sendToken(user, res) {
+    var token = jwt.sign(user.id, '123');
+    res.json({ firstName: user.firstName, token });
+}
+
+function sendAuthError(res) {
+    return res.json({ success: false, message: 'email or password incorrect' });
+}
+
+function checkAuthenticated(req, res, next) {
+    if(!req.header('authorization'))
+        return res.status(401).send({message: 'Unauthorized requested. Missing authentication header'});
+
+    var token = req.header('authorization').split(' ')[1];
+
+    var payload = jwt.decode(token, '123');
+
+    if(!payload)
+        return res.status(401).send({message: 'Unauthorized requested. Authentication header invalid'});
+
+    req.user = payload;
+
+    next();
+}
+
+app.use('/api', api);
+app.use('/auth', auth);
+
+
+app.listen(63145);var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+
+var messages = [{ text: 'some text', owner: 'Tim' }, { text: 'other message', owner: 'Jane' }];
+var users = [{firstName: 'admin', email: 'admin', password: 'admin', id: 0}];
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+})
+
+var api = express.Router();
+var auth = express.Router();
+
+
+api.get('/messages', (req, res) => {
+    res.json(messages);
+})
+
+api.get('/messages/:user', (req, res) => {
+    var user = req.params.user;
+    var result = messages.filter(message => message.owner == user);
+
+    res.json(result);
+})
+
+api.post('/messages', (req, res) => {
+    messages.push(req.body);
+    res.json(req.body);
+})
+
+api.get('/users/me', checkAuthenticated, (req,res) => {
+    res.json(users[req.user]);
+})
+
+api.post('/users/me', checkAuthenticated, (req,res) => {
+    var user = users[req.user];
+
+    user.firstName = req.body.firstName;
+    user.lastName = req.body.lastName;
+
+    res.json(user);
+})
+
+auth.post('/login', (req, res) => {
+    var user = users.find(user => user.email == req.body.email);
+
+    if (!user) 
+        sendAuthError(res);
+
+    if (user.password == req.body.password)
+        sendToken(user, res);
+    else
+        sendAuthError(res);
+})
+
+auth.post('/register', (req, res) => {
+    var index = users.push(req.body) - 1;
+
+    var user = users[index];
+    user.id = index;
+
+    sendToken(user, res);
+})
+
+function sendToken(user, res) {
+    var token = jwt.sign(user.id, '123');
+    res.json({ firstName: user.firstName, token });
+}
+
+function sendAuthError(res) {
+    return res.json({ success: false, message: 'email or password incorrect' });
+}
+
+function checkAuthenticated(req, res, next) {
+    if(!req.header('authorization'))
+        return res.status(401).send({message: 'Unauthorized requested. Missing authentication header'});
+
+    var token = req.header('authorization').split(' ')[1];
+
+    var payload = jwt.decode(token, '123');
+
+    if(!payload)
+        return res.status(401).send({message: 'Unauthorized requested. Authentication header invalid'});
+
+    req.user = payload;
+
+    next();
+}
+
+app.use('/api', api);
+app.use('/auth', auth);
+
+
+app.listen(4567);
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,42 +1,51 @@
-import { NgModule }      from '@angular/core';
-import { RouterModule } from "@angular/router";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { MessagesComponent } from './messages.component';
-import { NewMessageComponent } from './new-message.component';
 import { MaterialModule } from '@angular/material';
-import { HttpModule } from '@angular/http';
-import { AppComponent }  from './app.component';
+import { MessagesComponent } from './messages-component';
+import { AppComponent } from './app.component';
 import { WebService } from './web.service';
+import { NewMessageComponent } from './new-message.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NavComponent } from "./nav.component";
-import { HomeComponent } from "./home.component";
-import { RegisterComponent } from "./register.component";
-import { AuthService } from "./auth.service";
+import { NavComponent } from './nav.component';
+import { HomeComponent } from './home.component';
+import { RegisterComponent } from './register.component';
+import { AuthService } from './auth.service';
+import  { LoginComponent } from './login.component';
+import { UserComponent } from './user.component';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-var routes=[
+var routes = [
   {
-    path:'',
+    path: '',
     component: HomeComponent
   },
   {
-    path:'messages/:name',
+    path: 'messages',
     component: MessagesComponent
   },
   {
-    path:'register',
+    path: 'messages/:name',
+    component: MessagesComponent
+  },
+  {
+    path: 'register',
     component: RegisterComponent
   },
   {
-    path:'messages',
-    component: MessagesComponent
-  }
-];
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'user',
+    component: UserComponent
+  }];
 
 @NgModule({
-  imports:      [ ReactiveFormsModule,BrowserModule,HttpModule,MaterialModule,BrowserAnimationsModule,FormsModule,RouterModule.forRoot(routes)],
-  declarations: [ AppComponent, MessagesComponent,NavComponent,NewMessageComponent, HomeComponent, RegisterComponent],
-  bootstrap:    [ AppComponent ],
-  providers: [WebService]
+  imports: [BrowserAnimationsModule, HttpModule, BrowserModule, MaterialModule, FormsModule, ReactiveFormsModule, RouterModule.forRoot(routes)],
+  declarations: [AppComponent, MessagesComponent, NewMessageComponent, NavComponent, HomeComponent, RegisterComponent, LoginComponent, UserComponent],
+  bootstrap: [AppComponent],
+  providers: [WebService, AuthService]
 })
 export class AppModule { }
